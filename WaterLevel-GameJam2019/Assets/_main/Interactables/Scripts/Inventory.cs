afb7d0802d8c7328e2a17ddf4c8340db0fc78964
Retus001 <retus001@gmail.com>
2019-06-29 05:16:04 -0500

    Inventory Update
    Simple Lock / Key System
    +Interactables
    + Inventory
    + Locked Interactables

---
+++ WaterLevel-GameJam2019/Assets/_main/Interactables/Scripts/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> keyObjects = new List<GameObject>();
+
+    public GameObject target;
+
+    public void CollectInteractable(GameObject _interactable){
+        if (_interactable.GetComponent<Interactable>())
+            keyObjects.Add(_interactable);
+    }
+
+    public void ClearAllInteractables(){
+        keyObjects.Clear();
+    }
+
+    public void RemoveInteractable(GameObject _interactable){
+        keyObjects.Remove(_interactable);
+    }
+
+    public void CheckItemInteraction(GameObject _target){
+        bool unlocked = false;
+        for (int i = 0; i < keyObjects.Count; i++){
+            if (keyObjects[i].GetComponent<Interactable>().id == _target.GetComponent<LockedInteractable>().id){
+                _target.GetComponent<LockedInteractable>().Unlock();
+                unlocked = true;
+            }
+        }
+
+        if (unlocked)
+            Debug.Log("unlocked " + target.name);
+        else
+            Debug.Log("missing key object");
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.E)){
+            CheckItemInteraction(target);
+        }
+    }
+}
---
+++ WaterLevel-GameJam2019/Assets/_main/Interactables/Scripts/LockedInteractable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedInteractable : MonoBehaviour
+{
+    public int id;
+
+    public GameObject[] doors;
+
+    public void Unlock(){
+        for (int i = 0; i < doors.Length; i++){
+            doors[i].SetActive(false);
+        }
+    }
+
+}
