89934536ccf87b737456b9630ed26172ce17786b
Ngakk <ngakwastaken@gmail.com>
2019-06-29 01:23:11 -0500

    Cow Behaviour Update
    + Cow Behaviour
    + Cow Spawner
    + Cow Event
    
    WIP

---
+++ WaterLevel-GameJam2019/Assets/Cow/Scripts/CowBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowBehaviour : MonoBehaviour
+{
+    public float followSpeed;
+    public float followDistanceLimit;
+    public float followSpeedFalloff;
+
+    private GameObject player;
+    private float currentFalloff;
+
+    private bool willFollowPlayer = true;
+
+    public void SetCowStats(float _speed, float _fallOff, float _followDistanceLimit)
+    {
+        followSpeed = _speed;
+        followSpeedFalloff = _fallOff;
+        followDistanceLimit = _followDistanceLimit;
+    }
+
+    private void FollowPlayer()
+    {
+        if (willFollowPlayer)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) > followDistanceLimit)
+            {
+                if (currentFalloff < followSpeedFalloff)
+                    currentFalloff += Time.deltaTime;
+
+                transform.position = Vector3.Lerp(transform.position, player.transform.position, followSpeed - currentFalloff);
+            }
+            else
+                KillPlayer();
+        }
+    }
+
+    private void KillPlayer()
+    {
+        Destroy(player);
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        FollowPlayer();
+    }
+}
---
+++ WaterLevel-GameJam2019/Assets/Cow/Scripts/CowSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowSpawner : MonoBehaviour
+{
+    public AudioClip[] cowSFX;
+    public float nearestLimit;
+    public float farthestLimit;
+    public bool canSpawnRandom = false;
+
+    [Range(0,1)]
+    public float randomSpawnChance;
+    public float randomSpawnDelay;
+    private float currentTime;
+    private GameObject player;
+
+    // TEMP
+    [Header("TEMP")]
+    public GameObject TEMP_SoundDebugger;
+
+    public void EventSpawn(Vector3 _position)
+    {
+        SpawnCow(_position);
+    }
+
+    public void RandomSpawn()
+    {
+        if (currentTime <= 0)
+        {
+            if (Random.Range(0,1) > randomSpawnChance)
+            {
+                float spawnDistance = Random.Range(nearestLimit + farthestLimit / 2, nearestLimit + farthestLimit);
+                Vector3 spawnPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - spawnDistance);
+                SpawnCow(spawnPosition);
+            }
+            currentTime = randomSpawnDelay;
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
+    }
+
+    private void SpawnCow(Vector3 _position)
+    {
+        GameObject moo = Instantiate(TEMP_SoundDebugger);
+        float _distance = Vector3.Distance(player.transform.position, _position);
+
+        // TEMP Instantiate "Sound"
+        if (_distance < nearestLimit)
+        {
+            TEMP_SoundDebugger.name = "TEMP_Cow_" + cowSFX[0].name;
+        }
+        else if (_distance >= nearestLimit && _distance <= farthestLimit)
+        {
+            TEMP_SoundDebugger.name = "TEMP_Cow_" + cowSFX[1].name;
+        }
+        else if (_distance > farthestLimit)
+        {
+            TEMP_SoundDebugger.name = "TEMP_Cow_" + cowSFX[2].name;
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (canSpawnRandom)
+            RandomSpawn();
+    }
+}
