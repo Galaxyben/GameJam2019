540a497e899006c78cd1b79ea076db07acc00431
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2019-06-29 10:11:44 -0500

    Mini cambios para el Toggle

--- WaterLevel-GameJam2019/Assets/_main/Scripts/Character_Controller.cs
+++ WaterLevel-GameJam2019/Assets/_main/Scripts/Character_Controller.cs
@@ -5,6 +5,7 @@ using Rewired;
 
 public class Character_Controller : MonoBehaviour
 {
+    public bool debugMode = false;
     public Rigidbody rigi;
     public Camera camara;
     public int playerId = 0;
@@ -18,11 +19,15 @@ public class Character_Controller : MonoBehaviour
     private Quaternion characterRot;
     private Quaternion cameraRot;
     private bool isRuning = false;
+    bool toggleRuning;
 
     private void Awake() {
         player = ReInput.players.GetPlayer(playerId);
         rigi = gameObject.GetComponent<Rigidbody>();
+        Screen.lockCursor = true;
+        Cursor.visible = false;
     }
+ 
 
     // Update is called once per frame
     void Update()
@@ -37,19 +42,24 @@ public class Character_Controller : MonoBehaviour
         moveVector.z = player.GetAxis("Move Vertical");
         viewVector.x = player.GetAxis("View Horizontal");
         viewVector.y = player.GetAxis("View Vertical");
-        isRuning = player.GetButton("Correr");
+        isRuning = player.GetButton("Running");
     }
 
     private void ProcessInput()
     {   
+        if(isRuning)
+        {
+            toggleRuning = true;
+        }
         if(moveVector.x != 0.0f || moveVector.z != 0.0f)
         {
             moveVector *= moveSpeed;
-            rigi.velocity = (isRuning ? ((transform.right * moveVector.x) + (transform.forward * moveVector.z) + (transform.up * rigi.velocity.y)) * 2.0f : (transform.right * moveVector.x) + (transform.forward * moveVector.z) + (transform.up * rigi.velocity.y));
+            rigi.velocity = (toggleRuning ? ((transform.right * moveVector.x) + (transform.forward * moveVector.z) + (transform.up * rigi.velocity.y)) * 2.0f : (transform.right * moveVector.x) + (transform.forward * moveVector.z) + (transform.up * rigi.velocity.y));
         }
         if(moveVector.x == 0.0f && moveVector.z == 0.0f)
         {
-            rigi.velocity = Vector3.zero;
+            rigi.velocity = new Vector3(0.0f, rigi.velocity.y, 0.0f);
+            toggleRuning = false;
         }
         Vector3 tempMove = new Vector3();
         tempMove.x = Mathf.Clamp(viewVector.x, velocidadRotacionMin, velocidadRotacionMax);
@@ -57,7 +67,7 @@ public class Character_Controller : MonoBehaviour
         tempMove.Normalize();
         transform.Rotate(0.0f, tempMove.x , 0.0f);
         camara.transform.Rotate(tempMove.y, 0.0f, 0.0f);
-        if(player.GetButtonTimedPress("Interactuar", 0.2f))
+        if(player.GetButtonTimedPress("Iteractuable", 0.2f))
         {
             print("Estoy interactuando");
         }
