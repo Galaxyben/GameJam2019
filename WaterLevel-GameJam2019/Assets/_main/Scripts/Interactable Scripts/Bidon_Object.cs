92924744bc08ba485ef23e7e51a5be8323f47ba6
Galaxyben <galaxyben1@gmail.com>
2019-06-30 11:15:47 -0500

    Menu, triggers

--- WaterLevel-GameJam2019/Assets/_main/Scripts/Interactable Scripts/Bidon_Object.cs
+++ WaterLevel-GameJam2019/Assets/_main/Scripts/Interactable Scripts/Bidon_Object.cs
@@ -6,12 +6,17 @@ public class Bidon_Object : Objetos_Script
 {
     public Character_Controller player;
     public GameObject zoneTrigger;
+    public GameObject[] zoneTrigger2 = new GameObject[4];
 
     void Update()
     {
         if (isActivated)
         {
             zoneTrigger.SetActive(true);
+            for(int i = 0; i < zoneTrigger2.Length; i++)
+            {
+                zoneTrigger2[i].SetActive(true);
+            }
             player.actualState = Items.GASOLINA;
             Destroy(this.gameObject, 0.4f);
             print("Conseguiste Bidón!");
