b2029632568883b7dec9ed84c31370011f0117fe
Galaxyben <galaxyben1@gmail.com>
2019-06-29 20:41:35 -0500

    Before Super Merge

--- WaterLevel-GameJam2019/Assets/_main/Scripts/Objetos_Script.cs
+++ WaterLevel-GameJam2019/Assets/_main/Scripts/Objetos_Script.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BoxCollider))]
 public class Objetos_Script : MonoBehaviour
 {
     public Items dataCompare = Items.INICIO;
     public bool isActivated;
+    public UnityEvent evento;
 
     private void Awake() {
         gameObject.GetComponent<BoxCollider>().isTrigger = true;
